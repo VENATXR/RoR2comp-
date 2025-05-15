746a34fa488f20ce1b3a03db15b02fc395d334bd
VENATXR <blackdevil1703@mail.ru>
2025-05-15 21:15:42 +0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,22 @@
-const { getDefaultConfig } = require('metro-config');
-const { fileURLToPath } = require('url');
-const { dirname, resolve } = require('path');
+import { getDefaultConfig } from 'metro-config';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { resolve as resolveTransformer } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-module.exports = (async () => {
+export default (async () => {
   const {
-    resolver: { sourceExts, assetExts }
+    resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
 
   const config = {
     transformer: {
-      babelTransformerPath: require.resolve('react-native-babel-transformer'),
+      babelTransformerPath: resolveTransformer('react-native-babel-transformer'),
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
     watchFolders: [
@@ -24,7 +25,6 @@ module.exports = (async () => {
       resolve(__dirname, './assets/icons'),
       resolve(__dirname, './assets/images'),
       resolve(__dirname, './data'),
-      // Добавление .github/workflows не имеет смысла, так как там лежат файлы конфигурации CI/CD, а не код приложения
     ],
     excludeSource: [/node_modules\/.*/],
   };
